c949af8622a59e89b8ffcf65f01a00a3a85e1851
ianPineda01 <ianpineda@ciencias.unam.mx>
2022-11-21 13:14:49 -0600

    Add docstrings to flask route functions

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-#====================================Imports====================================
 from pyspark.sql import SparkSession
 from flask import Flask, render_template
 from flask_wtf import FlaskForm #type: ignore Stub file not found 
 from wtforms import StringField #type: ignore Stub file not found
 from wtforms.validators import DataRequired, URL #type: ignore Stub file not found
-from scraping import scrape
+
 import webbrowser
+
+from scraping import scrape
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'TO-DO: have a real key'
@@ -15,11 +16,19 @@ class MyForm(FlaskForm):
 
 @app.route('/')
 def index():
+    """
+    Flask index route, creates a flask_wtform to get the desired url for querying.
+    """
     form = MyForm()
     return render_template('index.html', form=form) 
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
+    """
+    Flask submit route, queries the website from the index form, scrapes it for
+    relevant data, and makes a spark dataframe with said data, finally returns a
+    table with the data gathered.
+    """
     spark = SparkSession.builder.appName('Real_Estate').getOrCreate()
 
     form = MyForm()
@@ -34,6 +43,13 @@ def submit():
     spark.stop()
     return resulting_html
 
-if __name__ == '__main__':
+def main():
+    """
+    Entry point to the app.
+    Opens a web browser at index page of web server, and starts flask server
+    """
     webbrowser.open('localhost:5000', new = 2)
     app.run(debug=True)
+
+if __name__ == '__main__':
+    main()
