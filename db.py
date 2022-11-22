9f84ce19f0cf27361903e0fc93776e84c80f80a1
ianPineda01 <ianpineda@ciencias.unam.mx>
2022-11-22 12:31:18 -0600

    Add functionality to read and erase current stored data

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession, DataFrame
 import pandas as pd
 
+from typing import Optional
 import sqlite3
 
 def write_DF(DF:DataFrame, db_name:str):
@@ -8,13 +9,26 @@ def write_DF(DF:DataFrame, db_name:str):
     Takes a spark dataframe and writes it's contents into a sqlite database
     """
     with sqlite3.connect(db_name) as conn:
-        DF.toPandas().to_sql('units', conn)
+        DF.toPandas().to_sql('units', conn, if_exists='append', index=False)
+        print(DF.toPandas())
+    
+def clear_data(db_name:str):
+    """
+    Clears all the data from database
+    """
+    with sqlite3.connect(db_name) as conn:
+        cursor = conn.cursor()
+        cursor.execute('DROP TABLE IF EXISTS units')
+        
 
-def read_to_DF(spark: SparkSession, db_name:str) -> DataFrame:
+def read_to_DF(spark: SparkSession, db_name:str) -> Optional[DataFrame]:
     """
     Reads the contents of a database and writes them into a spark dataframe
     """
     with sqlite3.connect(db_name) as conn:
-        df = pd.read_sql_query('SELECT * FROM units', conn)
+        try:
+            df = pd.read_sql_query('SELECT * FROM units', conn)
+        except pd.errors.DatabaseError:
+            return None
 
     return spark.createDataFrame(df)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm #type: ignore Stub file not found 
 from wtforms import StringField #type: ignore Stub file not found
 from wtforms.validators import DataRequired, URL #type: ignore Stub file not found
@@ -7,12 +7,14 @@ from wtforms.validators import DataRequired, URL #type: ignore Stub file not fou
 import webbrowser
 
 from scraping import scrape, html_from_url
-from db import write_DF
+from db import write_DF, read_to_DF, clear_data
+
+DATABASE_FILE = 'main.db'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'TO-DO: have a real key'
 
-class MyForm(FlaskForm):
+class FetchURL(FlaskForm):
     url = StringField('url:', validators=[DataRequired(), URL()])
 
 @app.route('/')
@@ -20,28 +22,49 @@ def index():
     """
     Flask index route, creates a flask_wtform to get the desired url for querying.
     """
-    form = MyForm()
+    form = FetchURL()
     return render_template('index.html', form=form) 
 
-@app.route('/submit', methods=['GET', 'POST'])
-def submit():
+
+@app.route('/clearData', methods=['GET', 'POST'])
+def clear_db():
     """
-    Flask submit route, queries the website from the index form, scrapes it for
+    """
+    clear_data(DATABASE_FILE)
+    return redirect('/')
+
+@app.route('/dbData', methods=['GET', 'POST'])
+def db_data():
+    """
+    Reads data from database and returns dataframe as html
+    """
+    spark = SparkSession.builder.appName('RealEstate').getOrCreate()
+    df = read_to_DF(spark, DATABASE_FILE)
+    if df is None:
+        return 'No data avaliable'
+    else:
+        return df.toPandas().to_html()
+
+@app.route('/fetchURL', methods=['GET', 'POST'])
+def fetch_URL():
+    """
+    Flask fetchURL route, queries the website from the index form, scrapes it for
     relevant data, and makes a spark dataframe with said data, finally returns a
     table with the data gathered.
     """
     spark = SparkSession.builder.appName('Real_Estate').getOrCreate()
 
-    form = MyForm()
+    form = FetchURL()
 
-    html:str = html_from_url(form.url.data) #type: ignore
+    url:str = form.url.data
+    html:str = html_from_url(url)
     data = scrape(html)
 
     df = spark.createDataFrame(data, ['Size', 'Price (MXN)', 'Link'])
     df2 = df.withColumn('Price/m^2', df['Price (MXN)'] / df['Size'])
 
-    write_DF(df2, 'main.db')
-    resulting_html = df2.toPandas().to_html(index=False) #type: ignore 
+    write_DF(df2, DATABASE_FILE)
+    resulting_html = df2.toPandas().to_html(index=False) 
     spark.stop()
     return resulting_html
 
